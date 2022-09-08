63dd87a6c80d78b9df8a6d70c7e96844815a86e1
Kylesk22 <105800286+Kylesk22@users.noreply.github.com>
2022-09-07 20:31:25 -0400

    Nutrition Update

--- src/Pages/Nutrition/Nutrition.jsx
+++ src/Pages/Nutrition/Nutrition.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "./Nutrition.css";
 
 const Nutrition = () => {
@@ -7,6 +8,7 @@ const Nutrition = () => {
         <div>
             <h1>Nutrition</h1>
         </div>
+
     )
 }
 
